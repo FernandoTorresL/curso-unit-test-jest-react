bb4f0659291e5b5a09bb00d47cdb7a7d897cf187
Fer Torres <fertorresmx@gmail.com>
2020-03-23 02:19:55 -0600

    Add new test and mocks.

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -3,11 +3,11 @@ import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
-	const footer = mount(<Footer />);
-	test('Render del componente Footer', () => {
-		expect(footer.length).toEqual(1);
-	});
-	test('Render del título', () => {
-		expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
-	});
+  const footer = mount(<Footer />);
+  test('Render del componente Footer', () => {
+    expect(footer.length).toEqual(1);
+  });
+  test('Render del título', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
 });
